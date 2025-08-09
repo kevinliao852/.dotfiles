a6f87006783dec40ccf258756925c352edda1fb9
Kevin Liao <q85292542000@gmail.com>
2025-08-09 09:29:28 +0800

    ✨ Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+brew install btop
+brew install pre-commit
+brew install autojump
+brew install ripgrep
+brew install docker
+brew install docker-compose
+
+# TUI
+brew install k9s
+brew install tig
+brew install tmux
+brew install lazygit
+brew install yazi
