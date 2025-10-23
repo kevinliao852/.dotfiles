1fab56de7e28fcce3341dfd8ae1aef8cbc18901e
kevinliao852 <q85292542000@gmail.com>
2025-10-23 19:08:35 +0800

    ✨ Add mode brew pkgs

--- brew.sh
+++ brew.sh
@@ -13,3 +13,12 @@ brew install tig
 brew install tmux
 brew install lazygit
 brew install yazi
+brew isntall gitmoji
+
+# Zsh tools
+
+brew install zsh-autosuggestions
+brew install autojump
+brew install fzf
+brew install ripgrep
+
